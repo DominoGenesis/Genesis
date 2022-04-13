d7c45cae0ea1b6c08f72d708cad17b37c9708bac
Dmytro <dpastov@gmail.com>
2022-04-13 15:04:11 +0200

    corrected dependencies

--- src/main/java/net/prominic/genesis/JSONRules.java
+++ src/main/java/net/prominic/genesis/JSONRules.java
@@ -1,7 +1,6 @@
 package net.prominic.genesis;
 
 import java.io.File;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.net.URL;
@@ -18,8 +17,8 @@ import lotus.domino.Database;
 import lotus.domino.DocumentCollection;
 import lotus.domino.Document;
 import lotus.domino.NotesException;
-import net.prominic.gja_v20220411.GLogger;
-import net.prominic.gja_v20220411.ProgramConfig;
+import net.prominic.gja_v20220413.GLogger;
+import net.prominic.gja_v20220413.ProgramConfig;
 import net.prominic.utils.DominoUtils;
 import net.prominic.utils.HTTP;
 
