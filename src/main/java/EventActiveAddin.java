e2120fc0872e929510ec14666840b9d5f8ce5dec
Dmytro <dpastov@gmail.com>
2022-06-06 12:11:42 +0200

    new version

---
+++ src/main/java/EventActiveAddin.java
@@ -0,0 +1,80 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Date;
+
+import org.json.simple.JSONObject;
+
+import net.prominic.gja_v080.Event;
+import net.prominic.gja_v080.GConfig;
+import net.prominic.gja_v080.GLogger;
+import net.prominic.util.FileUtils;
+
+public class EventActiveAddin extends Event {
+	public String JavaAddinRoot = null;
+	public String JavaAddinConfig = null;
+	public String JavaAddinLive = null;
+	public String Catalog = null;
+	public String Server = null;
+
+	public EventActiveAddin(String name, long seconds, boolean fireOnStart, GLogger logger) {
+		super(name, seconds, fireOnStart, logger);
+	}
+
+	@Override
+	public void run() {
+		try {
+			File file = new File(JavaAddinRoot);
+			if (!file.exists()) return;
+
+			String osName = System.getProperty("os.name").toLowerCase();
+			boolean inWindows = osName.contains("win");
+
+			File[] directories = file.listFiles();
+			for(int i=0; i<directories.length; i++) {
+				if (directories[i].isDirectory()) {
+					String addinName = directories[i].getName();
+					String addinConfigPath = JavaAddinRoot + File.separator + addinName + File.separator + JavaAddinConfig;
+					String addinLivePath = JavaAddinRoot + File.separator + addinName + File.separator + JavaAddinLive;
+					String addinActive = GConfig.get(addinConfigPath, "active");
+					String runjava = GConfig.get(addinConfigPath, "runjava");
+
+					System.out.println("addinActive="+ addinActive);
+					System.out.println("isLive(addinLivePath)="+ String.valueOf(isLive(addinLivePath)));
+					if ("1".equals(addinActive) && !isLive(addinLivePath)) {
+						String runjavaTask = inWindows ? "nrunjava" : "runjava";
+
+						String cmd = String.format("%s %s", runjavaTask, runjava);
+						System.out.println("cmd="+ cmd);
+						
+						@SuppressWarnings("unused")
+						Process proc = Runtime.getRuntime().exec(cmd);
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}					
+
+	}
+
+	private boolean isLive(String javaAddinPath) {
+		File f = new File(javaAddinPath);
+		if (!f.exists()) return false;
+
+		String sTimeStamp = FileUtils.readFile(f);
+		if (sTimeStamp.length() == 0) return false;
+
+		// last live date
+		long timeStamp = Long.parseLong(sTimeStamp);
+		Date date1 = new Date(timeStamp);
+		Calendar c1 = Calendar.getInstance();
+		c1.setTime(date1);
+		c1.add(Calendar.HOUR, 1);
+
+		// current date
+		Calendar c2 = Calendar.getInstance();
+
+		return c1.after(c2);
+	}
+}
